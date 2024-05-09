a1b2298682b3961e2ea0a958b4fe1d857da73742
Mesh-254 <meshack1995@gmail.com>
2024-05-09 23:28:07 +0300

    created fucntions to receive message from server

--- src/client.py
+++ src/client.py
@@ -1,28 +1,40 @@
 import socket
-import time
 
-HEADERSIZE = 10
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+FORMAT = 'UTF-8'
+HEADERSIZE = 1024
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
 
-full_msg = ''
-new_msg = True
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER, PORT))
 
-while True:
-    msg = s.recv(20).decode('utf-8')
-    if new_msg:
-        print(f'new message length: {msg[:HEADERSIZE]}')
-        msglen =  int(msg[:HEADERSIZE])
-        new_msg = False
+def  send_search_query(search_query):
+    search_query_length = len(search_query)
+    send_length = str(search_query_length).encode(FORMAT)
+    send_length += b'\n' * (HEADERSIZE - len(send_length))
+    client_socket.send(send_length)
+    client_socket.send(search_query.encode(FORMAT))
 
-    full_msg += msg
-
-    if len(full_msg) - HEADERSIZE ==  msglen:
-        print('Full messages received')
-        print(full_msg[HEADERSIZE:])
-        new_msg = True
-        full_msg = ''
- 
-
+def receive_message(client_socket):
+    message_header = client_socket.recv(HEADERSIZE).decode(FORMAT)
+    if not message_header:
+        print(f"Client disconnected")
+        return False
+    try:
+        message_length = int(message_header.strip())
+    except ValueError:
+        print(f"Invalid message header from :{message_header}")
+        
+    message = client_socket.recv(message_length).decode(FORMAT)
+    print(f'[Received message:] {message_length} : {message}')
     
+# Continuously accept user input and send messages to the server
+while True:
+    search_text = input("Enter your message ('exit' to quit): ")
+    if search_text.lower() == 'exit':
+        break
+    send_search_query(search_text)
+    response = receive_message(client_socket)
+    print(response)
+
