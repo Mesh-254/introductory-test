27fc64599383d467ff853bf00834203577553925
Mesh-254 <meshack1995@gmail.com>
2024-05-10 17:13:45 +0300

    Update The path to the file is read from a configuration file. The server listens on a specified port and responds

--- src/client.py
+++ src/client.py
@@ -11,23 +11,26 @@ client_socket.connect((SERVER, PORT))
 
 def  send_search_query(search_query):
     search_query_length = len(search_query)
-    send_length = str(search_query_length).encode(FORMAT)
+    send_length = str(search_query_length).encode(FORMAT).strip()
     send_length += b'\n' * (HEADERSIZE - len(send_length))
     client_socket.send(send_length)
     client_socket.send(search_query.encode(FORMAT))
 
-def receive_message(client_socket):
+def receive_message():
+   
     message_header = client_socket.recv(HEADERSIZE).decode(FORMAT)
     if not message_header:
-        print(f"Client disconnected")
-        return False
+        print(f"Client {client_socket} disconnected")
+
     try:
         message_length = int(message_header.strip())
-    except ValueError:
-        print(f"Invalid message header from :{message_header}")
         
-    message = client_socket.recv(message_length).decode(FORMAT)
-    print(f'[Received message:] {message_length} : {message}')
+    except ValueError:
+        print(f"Invalid message header:{message_header}")
+    
+    finally:
+        message = client_socket.recv(message_length).decode(FORMAT)
+        return f'[Received message from server:] {message_length} : {message}'
     
 # Continuously accept user input and send messages to the server
 while True:
@@ -35,6 +38,6 @@ while True:
     if search_text.lower() == 'exit':
         break
     send_search_query(search_text)
-    response = receive_message(client_socket)
+    response = receive_message()
     print(response)
 
