de933a85293e448ddb27fd3152a6ba458655e8ec
Mesh-254 <meshack1995@gmail.com>
2024-05-12 13:34:31 +0300

    added ip address and sys err and output to log information in logs files

--- src/server.py
+++ src/server.py
@@ -8,13 +8,16 @@ import threading
 import sys
 import time  # Importing time for time-related operations
 import ssl  # Importing ssl for secure socket layer operations
-from typing import Tuple  # Importing typing for type hints
+from typing import Tuple, List # Importing typing for type hints
 
 FORMAT = 'UTF-8'  # Setting the encoding format for communication
 HEADERSIZE = 1024  # Setting the header size for messages
 PORT = 5050  # Setting the port number for the server
 # Getting the server IP address
-SERVER = socket.gethostbyname(socket.gethostname())
+SERVER='192.168.0.102'
+
+# SERVER = socket.gethostbyname(socket.gethostname())
+
 
 # SSL configuration
 # USE_SSL = True  # Set to False to disable SSL (for testing)
@@ -32,7 +35,7 @@ def read_config() -> str:
         FileNotFoundError: If the configuration file is not found.
     """
     try:
-        with open('./config.ini', 'r') as f:
+        with open('/home/frosty/projects/introductory-test/config.ini', 'r') as f:
             # Iterating through the lines in the configuration file
             for data in f:
                 if data.startswith(
@@ -42,7 +45,7 @@ def read_config() -> str:
                     return file_path  # Returning the file path
 
     except FileNotFoundError as e:  # Handling file not found error
-        print("Error accessing config file : ", str(e), "\n")
+        sys.stderr.write("Error accessing config file : {}\n".format(str(e)))
         sys.exit()  # Exiting the program if config file is not found
 
 
@@ -64,12 +67,11 @@ def fetch_file_data(file_path: str) -> str:
             file_data = f.read()  # Reading the contents of the file
             return file_data  # Returning the file data
     except FileNotFoundError as e:  # Handling file not found error
-        print("file in path was not found.", str(e))
+        sys.stderr.write("file in path was not found: {}\n".format(str(e)))
         sys.exit()  # Exiting the program if file is not found
     except Exception as e:  # Handling other exceptions
-        print("An error occurred while reading the file.", str(e))
+        sys.stderr.write("An error occurred while reading the file: {}\n".format(str(e)))
         sys.exit()  # Exiting the program if an error occurs
-
 
 def find_string_match(
         message: str, REREAD_ON_QUERY: bool = True) -> Tuple[str, float, str]:
@@ -98,26 +100,20 @@ def find_string_match(
         file_data = file_data  # Storing file data
 
     start_time = time.time()  # Recording the start time of search
-    # Split file data into lines and search for the message
-    for line in file_data.splitlines(
-    ):  # Iterating through each line in the file
-        if message.strip() == line.strip():  # Checking for full string match
+        
+    # Perform KMP string search for each line in the file
+    for line in file_data.splitlines():
+       
+        if message.strip() in line:
             end_time = time.time()  # Recording the end time
             time_taken = end_time - start_time  # Calculating the time taken
-            current_time = time.strftime(
-                '%Y-%m-%d %H:%M:%S',
-                time.localtime())  # Getting current timestamp
-            # Returning match result, time taken, and timestamp
-            return 'STRING EXISTS\n', time_taken, current_time
+            current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())  # Getting current timestamp
+            return 'STRING EXISTS\n', time_taken, current_time  # Returning match result, time taken, and timestamp
 
     end_time = time.time()  # Recording the end time
     time_taken = end_time - start_time  # Calculating the time taken
-    current_time = time.strftime(
-        '%Y-%m-%d %H:%M:%S',
-        time.localtime())  # Getting current timestamp
-    # Returning match result, time taken, and timestamp
-    return 'STRING NOT FOUND\n', time_taken, current_time
-
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())  # Getting current timestamp
+    return 'STRING NOT FOUND\n', time_taken, current_time  # Returning match result, time taken, and timestamp
 
 def handle_clients(client_socket: socket.socket, address: tuple) -> None:
     """
@@ -131,8 +127,7 @@ def handle_clients(client_socket: socket.socket, address: tuple) -> None:
         None
     """
     try:
-        print(f'Server established new connection from {
-              address}')  # Printing client connection info
+        sys.stdout.write(f'Server established new connection from {address}\n')  # Printing client connection info
 
         # Wrap client socket with SSL if enabled
         # if USE_SSL:
@@ -147,8 +142,7 @@ def handle_clients(client_socket: socket.socket, address: tuple) -> None:
         if path is None:
             # Raising error if file path is not found
             raise ValueError("File Path not found.")
-        print(f'This is the path contained in config file: {
-              path}')  # Printing file path
+        sys.stdout.write(f'This is the path contained in config file: {path}\n')  # Printing file path
 
         connected = True  # Flag to indicate client connection status
         while connected:
@@ -159,31 +153,28 @@ def handle_clients(client_socket: socket.socket, address: tuple) -> None:
                 return False  # Returning if header length is zero
             if not message_header:  # Checking if header is empty
                 # Printing client disconnection info
-                print(f"Client {address} disconnected")
+                sys.stdout.write(f"Client {address} disconnected\n")
                 break  # Breaking the loop if client disconnects
             try:
                 # Converting message header to integer
                 message_length = int(message_header.strip())
             except ValueError:
                 # Handling invalid message header
-                print(f"Invalid message header from {
-                      address}:{message_header}")
+                sys.stderr.write(f"Invalid message header from {address}:{message_header}\n")
                 # Continuing to next iteration if message header is invalid
                 continue
             # Receiving message from client
             message = client_socket.recv(message_length).decode(
                 FORMAT)
             # Printing received message from client
-            print(f'[Received string from Client {address}:]{
-                  message_length} : {message}')
+            print(f'[Received string from Client {address}:]{message_length} : {message}\n')
 
             # Search for the match in the file using the received search query
             string_match, time_taken, current_time = find_string_match(
                 message)  # Searching for string match
             if string_match:  # Checking if string match is found
-                print(string_match)  # Printing string match
-
-                print(current_time)  # Printing current timestamp
+                sys.stdout.write(string_match)  # Printing string match
+                sys.stdout.write(current_time + '\n')  # Printing current timestamp
 
                 # If time taken is less than 1 second, consider it as
                 # milliseconds
@@ -191,11 +182,11 @@ def handle_clients(client_socket: socket.socket, address: tuple) -> None:
                     # Converting time to milliseconds
                     time_taken_milliseconds = time_taken * 1000
                     # Printing time taken in milliseconds
-                    print(
-                        f'Time taken: {time_taken_milliseconds} milliseconds')
+                    sys.stdout.write(
+                        f'Time taken: {time_taken_milliseconds} milliseconds\n')
                 else:
                     # Printing time taken in seconds
-                    print(f'Time taken: {time_taken} seconds')
+                    sys.stdout.write(f'Time taken: {time_taken} seconds\n')
 
             # Code to send the string match to client
             string_length = len(string_match)  # Getting length of string match
@@ -209,7 +200,7 @@ def handle_clients(client_socket: socket.socket, address: tuple) -> None:
 
     except Exception as e:  # Handling exceptions
         # Printing error message
-        print(f"Error handling client {address}: {e}")
+        sys.stderr.write(f"Error handling client {address}: {e}\n")
         sys.exit()  # Exiting the program
 
 
@@ -225,10 +216,10 @@ def start_server() -> None:
     server_socket.setsockopt(
         socket.SOL_SOCKET,
         socket.SO_REUSEADDR,
-        1)  # Setting socket options
+        1)  # Setting socket options to Reload/reatart always
     server_socket.bind((SERVER, PORT))  # Binding server to address and port
     server_socket.listen()  # Starting to listen for connections
-    print(f'Server is listening at {SERVER}')  # Printing server listening info
+    print(f'Server is listening at {SERVER}\n')  # Printing server listening info
     while True:  # Infinite loop to accept client connections
         # Accepting client connection
         client_socket, address = server_socket.accept()
@@ -240,7 +231,7 @@ def start_server() -> None:
                 client_socket, address))
         thread.start()  # Starting the thread
         # Printing active connections count
-        print(f'[active connections:]{threading.active_count() - 1}')
+        sys.stdout.write(f'[active connections:]{threading.active_count() - 1}\n')
 
 
 if __name__ == '__main__':
