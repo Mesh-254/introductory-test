78482ffcd5945da5b1bf6929c899faa020ee2050
Mesh-254 <meshack1995@gmail.com>
2024-05-16 01:59:04 +0300

    added karp search method

--- search_algorithms/karp_algorithm.py
+++ search_algorithms/karp_algorithm.py
@@ -1,11 +1,13 @@
 import time
 import sys
+from typing import Tuple
+from src.server import read_config, fetch_file_data
 
 
-def karp_rabin_string_match(pattern: str, text: str) -> tuple:
+def karp_rabin_string_match(pattern: str, text: str) -> Tuple[str, float, str]:
     """
     Performs Karp-Rabin String Matching to check
-      if pattern matches any line in text.
+    if pattern matches any line in text.
 
     Args:
         pattern (str): The pattern to search for.
@@ -15,35 +17,25 @@ def karp_rabin_string_match(pattern: str, text: str) -> tuple:
         tuple: A tuple containing the search result,
         time taken to find the match, and the current timestamp.
     """
-    prime = 101  # A prime number to reduce hash collisions
-    pattern_length = len(pattern)  # Length of the pattern
-    text_length = len(text)  # Length of the text
-    pattern_hash = hash(pattern)  # Hash value of the pattern
+    pattern_lines = pattern.splitlines()
+    text_lines = text.splitlines()
+    pattern_length = len(pattern_lines)  # Length of the pattern
+    text_length = len(text_lines)  # Length of the text
     start_time = time.time()  # Recording the start time of search
-
-    # Calculate hash of pattern and first window of text
-    text_hash = hash(text[:pattern_length])
 
     # Iterate through the text
     for i in range(text_length - pattern_length + 1):
-        if pattern_hash == text_hash:
-            # Check if the substrings match
-            if pattern == text[i:i + pattern_length]:
-                end_time = time.time()  # Recording the end time
+        # Check if the substrings match
+        if pattern_lines == text_lines[i:i + pattern_length]:
+            end_time = time.time()  # Recording the end time
 
-                # Calculating the time taken
-                time_taken = end_time - start_time
-                current_time = time.strftime(
-                    '%Y-%m-%d %H:%M:%S',
-                    time.localtime())  # Getting current timestamp
-                # Returning match result, time taken, and timestamp
-                return 'STRING EXISTS\n', time_taken, current_time
-
-        # Calculate hash for the next window of text
-        if i < text_length - pattern_length:
-            text_hash = (prime * (text_hash - ord(text[i]) * (prime ** (
-                pattern_length - 1))) +
-                ord(text[i + pattern_length])) % sys.maxsize
+            # Calculating the time taken
+            time_taken = end_time - start_time
+            current_time = time.strftime(
+                '%Y-%m-%d %H:%M:%S',
+                time.localtime())  # Getting current timestamp
+            # Returning match result, time taken, and timestamp
+            return 'STRING EXISTS\n', time_taken, current_time
 
     # If no match is found, record end time, calculate time taken, and get
     # current timestamp
@@ -58,7 +50,7 @@ file_data = None
 
 
 def find_string_match(
-        message: str, REREAD_ON_QUERY: bool = False) -> tuple:
+        message: str, REREAD_ON_QUERY: bool = False) -> Tuple[str, float, str]:
     """
     Searches for a full match of a string in a file.
 
