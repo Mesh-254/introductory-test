d396d99a16d3fd184c1f2c78c6c8fef570b8753f
Mesh-254 <meshack1995@gmail.com>
2024-05-16 02:02:36 +0300

    added linear search method

--- search_algorithms/linear_search.py
+++ search_algorithms/linear_search.py
@@ -1,3 +1,7 @@
+import time
+from src.server import fetch_file_data, read_config
+
+
 def linear_search(text: str, pattern: str) -> bool:
     """
     Performs linear search to find a whole
@@ -22,3 +26,48 @@ def linear_search(text: str, pattern: str) -> bool:
             return True  # Whole line matching found
 
     return False  # Whole line matching not found
+
+
+file_data = None
+
+
+def find_string_match(
+        message: str, REREAD_ON_QUERY: bool = False) -> tuple:
+    """
+    Searches for a full match of a string in a file.
+
+    Args:
+        message (str): The string to search for.
+        REREAD_ON_QUERY (bool): Whether to re-read the file on every query.
+
+    Returns:
+        tuple: A tuple containing the search result,
+        time taken to find the match, and the current timestamp.
+    """
+
+    # Declare file_data as a global variable
+    global file_data
+
+    file_path = read_config()  # Getting the file path from configuration
+
+    # Re-read the file on every query or if file_data is not present
+    if REREAD_ON_QUERY or file_data is None:
+        file_data = fetch_file_data(file_path)
+
+    start_time = time.time()  # Recording the start time of search
+
+    # Call linear_search function to search for the pattern in the file data
+    result = linear_search(file_data, message)
+
+    end_time = time.time()  # Recording the end time
+
+    # Calculating the time taken
+    time_taken = end_time - start_time
+
+    # Returning match result, time taken, and timestamp
+    if result:
+        return 'STRING EXISTS\n', time_taken, time.strftime(
+            '%Y-%m-%d %H:%M:%S', time.localtime())
+    else:
+        return 'STRING NOT FOUND\n', time_taken, time.strftime(
+            '%Y-%m-%d %H:%M:%S', time.localtime())
