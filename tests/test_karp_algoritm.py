189eeeaaa08a921fafefd68c8f9885ad1e3ae916
Mesh-254 <meshack1995@gmail.com>
2024-05-16 13:49:14 +0300

    update tests karp algorithm

--- tests/test_karp_algoritm.py
+++ tests/test_karp_algoritm.py
@@ -115,34 +115,52 @@ def test_find_string_match_pattern_found(monkeypatch):
 
 
 def test_find_string_match_execution_time(monkeypatch):
-    """
-    Test case for measuring execution time.
-    """
-    # Measure execution time for different file sizes
-    file_sizes = [10000, 50000, 100000, 500000, 1000000]  # Lines in the file
+    file_sizes = [10000, 50000, 100000, 500000, 1000000]
+
     for size in file_sizes:
-        with monkeypatch.context() as m:
-            m.setattr("src.server.read_config", mock_read_config)
-            m.setattr("src.server.fetch_file_data", mock_fetch_file_data)
         try:
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                file_data = ("Line 1\nLine 2\n" * size).encode('utf-8')
+                tmp_file.write(file_data)
+                tmp_file_path = tmp_file.name
+
+            def mock_read_config():
+                return tmp_file_path
+
+            def mock_fetch_file_data(file_path):
+                with open(file_path, 'r') as f:
+                    return f.read()
+
+            monkeypatch.setattr("src.server.read_config", mock_read_config)
+            monkeypatch.setattr(
+                "src.server.fetch_file_data",
+                mock_fetch_file_data)
 
             start_time = time.time()
-            result = find_string_match("pattern", REREAD_ON_QUERY=False)
+            result, time_taken, current_time = find_string_match(
+                "Line 1\nLine 2", REREAD_ON_QUERY=False)
             end_time = time.time()
+
             time_taken = (end_time - start_time) * 1000
-            logger.info(
-                f"File size: {size} rows, Execution time: {
-                    time_taken:.4f} milliseconds")
+
+            logger.info(f"File size: {size}, Execution time: {
+                        time_taken} milliseconds")
         except Exception as e:
+            # Logging any exceptions that occur during the test
             logger.error(f"Exception during stress test: {e}")
         finally:
-            continue
+            os.remove(tmp_file_path)
 
 
 def test_find_string_match_stress_test(monkeypatch, caplog):
-    """
-    Stress test for the find_string_match function.
-    """
+
+    def mock_read_config():
+        return tmp_file_path
+
+    def mock_fetch_file_data(file_path):
+        with open(file_path, 'r') as f:
+            return f.read()
+
     # Setting up mock functions
     monkeypatch.setattr("src.server.read_config", mock_read_config)
     monkeypatch.setattr("src.server.fetch_file_data", mock_fetch_file_data)
@@ -155,14 +173,18 @@ def test_find_string_match_stress_test(monkeypatch, caplog):
     for file_size in file_sizes:
         for query_count in query_counts:
             try:
-                # Creating temporary file with specified content
-                tmp_file_path = create_temp_file(
-                    "Line 1\n Karp algorithm \nLine 2\n" * file_size)
+                with tempfile.NamedTemporaryFile(delete=False) as file:
+                    file_data = (
+                        "Line 1\nTwo way Search \nLine 2\n" *
+                        file_size).encode('utf-8')
+                    file.write(file_data)
+                    tmp_file_path = file.name
 
                 start_time = time.time()
+
                 # Invoking find_string_match with a sample query
                 for _ in range(query_count):
-                    result = find_string_match("Line 2", REREAD_ON_QUERY=True)
+                    result = find_string_match("Line 2", REREAD_ON_QUERY=False)
 
                 end_time = time.time()
 
@@ -170,14 +192,15 @@ def test_find_string_match_stress_test(monkeypatch, caplog):
                 total_time_taken = (end_time - start_time) * 1000 / query_count
 
                 # Logging execution time
-
-                logger.info(f"File size: {file_size} rows, Queries per second:"
+                logger.info(f"File size: {file_size}, Queries per second:"
                             f"{query_count}, Average execution time per query:"
                             f"{total_time_taken:.4f} milliseconds")
 
                 # Asserting that the result indicates string exists
                 assert result[0] == "STRING EXISTS\n"
+
             except Exception as e:
+
                 # Logging any exceptions that occur during the test
                 logger.error(f"Exception during stress test: {e}")
 
