6dae8568e9ae72162090617d33789a61a1b9c03f
Mesh-254 <meshack1995@gmail.com>
2024-05-16 04:30:01 +0300

    added client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,11 +7,15 @@ SEARCH_QUERY = "test query"
 MESSAGE = "test message"
 
 # Mock socket object
+
+
 @pytest.fixture
 def mock_socket():
     return MagicMock()
 
 # Test send_search_query function
+
+
 def test_send_search_query(mock_socket):
     # Arrange
     send_length = b'10\n'
@@ -23,11 +27,13 @@ def test_send_search_query(mock_socket):
     # Assert
     mock_socket.send.assert_called()
     assert mock_socket.send.call_count == 2
-   
+
     # Check the second call to client_socket.send
     mock_socket.send.assert_any_call(search_query)
 
 #   Test receive_message function
+
+
 def test_receive_message(mock_socket):
     # Arrange
     message_header = b'10\n'
@@ -48,15 +54,19 @@ def test_receive_message(mock_socket):
     mock_socket.recv.side_effect = [b'invalid_header\n']
     with pytest.raises(Exception) as exception:
         receive_message(mock_socket)
-    assert str(exception.value) == "Error receiving message: Invalid message header"
+    assert str(
+        exception.value) == "Error receiving message: Invalid message header"
 
     # Test receiving a message with incorrect length
     mock_socket.recv.side_effect = [b'0\n']
     with pytest.raises(Exception) as exception:
         receive_message(mock_socket)
-    assert str(exception.value) == "Error receiving message: Error reading message from server"
+    assert str(
+        exception.value) == "Error receiving message"
 
 # Integration test (simulated interaction between client and server)
+
+
 def test_integration(mock_socket):
     # Arrange
     message_header = b'10\n'
@@ -71,6 +81,7 @@ def test_integration(mock_socket):
 
     # Assert
     assert received_message == f'[Received message from server] 10 : {MESSAGE}'
+
 
 # Run the tests
 if __name__ == "__main__":
