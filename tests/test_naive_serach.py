2b992d1a4fbfd60713dc4a085ee3af0409f1c5b0
Mesh-254 <meshack1995@gmail.com>
2024-05-26 09:22:36 +0300

    update naive search  algorithm

--- tests/test_naive_serach.py
+++ tests/test_naive_serach.py
@@ -74,7 +74,7 @@ def test_multiline_text():
 
 
 def test_find_string_match_execution_time(monkeypatch):
-    file_sizes = [10000, 50000, 100000, 500000, 1000000]
+    file_sizes = [10000, 50000, 100000, 250000, 500000, 1000000]
 
     for size in file_sizes:
         try:
@@ -102,8 +102,9 @@ def test_find_string_match_execution_time(monkeypatch):
 
             time_taken = (end_time - start_time) * 1000
 
-            logger.info(f"File size: {size}, Execution time: {
-                        time_taken} milliseconds")
+            logger.info(f'File size: {size}, Execution time:'
+                        f'{time_taken} milliseconds'
+                        )
         except Exception as e:
             # Logging any exceptions that occur during the test
             logger.error(f"Exception during stress test: {e}")
